58bd2b188d979d47d8e09bbcfc2d2987b47047f4
Max Mackie <maxmackie13@gmail.com>
2021-11-04 22:45:24 +0000

    adding CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // App.js
 import React, { Component } from "react";
-import { Loading } from "./components/loading";
-import { Tours } from "./tours";
+// import { Loading } from "./components/loading";
+import { Tours } from "./components/Tours";
 const url = "https://course-api.netlify.app/api/react-tours-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
   return (
     <>
       <h2>tours project</h2>
